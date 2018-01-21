08e7e1577881adceb2b3acd3727d7f84b0114f06
Orlov Valentine <xunto@users.noreply.github.com>
2018-01-21 19:09:32 +0300

    Update update.js

--- remove_player_npc/update.js
+++ remove_player_npc/update.js
@@ -1,4 +1,11 @@
+TICKS = 0;
+
 function remove_player_update(event, world, npc) {
+    TICKS++;
+    
+    if (TICKS < 30) return;
+    TICKS = 0;
+
     var players = world.getAllServerPlayers();
     for (var i=0; i<players.length; i++) {
         if (npc.getName() == players[i].getName()) {
