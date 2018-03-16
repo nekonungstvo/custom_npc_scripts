803efeff526324031cee67c9c7258700c20bf261
Orlov Valentine <xunto@users.noreply.github.com>
2018-03-16 14:32:38 +0300

    Update farm_npc.js

--- farm_animal_npc/farm_npc.js
+++ farm_animal_npc/farm_npc.js
@@ -2,17 +2,19 @@ function farm_interact(npc, player) {
   var hours = 192;
 
   var now = new Date();
-  var last = new Date();
+  var last = null;
 
   if (npc.hasStoredData("farm")) {
     last = new Date(npc.getStoredData("farm"));
   }
 
-  var left = hours - ((now-last) / 3600000).toFixed(2);
-  if (left > 0) {
-    npc.say(player, "Последний сбор: " + last);
-    npc.say(player, "Следующий сбор через " + left + " часов")
-    return;
+  if (last != null) {
+    var left = hours - ((now-last) / 3600000).toFixed(2);
+    if (left > 0) {
+      npc.say(player, "Последний сбор: " + last);
+      npc.say(player, "Следующий сбор через " + left + " часов")
+      return;
+    }
   }
 
   var item = npc.getProjectileItem();
